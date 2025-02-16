40028382a02029714591f75373e9597fb8bc0033
Nasrulhaq Hidayat Abdul Latiff <nasrulhaqhidayat@gmail.com>
2025-02-16 19:59:42 +0800

    add descrioption

--- src/components/Body/Team.vue
+++ src/components/Body/Team.vue
@@ -5,9 +5,9 @@
             <div class="text-center mx-auto pb-5 wow fadeInUp" data-wow-delay="0.2s" style="max-width: 800px;">
                 <h4 class="text-primary">Members</h4>
                 <h1 class="display-4 mb-4">Meet Our Expert Team Members</h1>
-                <p class="mb-0">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur adipisci facilis
-                    cupiditate recusandae aperiam temporibus corporis itaque quis facere, numquam, ad culpa deserunt
-                    sint dolorem autem obcaecati, ipsam mollitia hic.
+                <p class="mb-0">Our team is made up of passionate professionals dedicated to making <strong>MudahJe</strong> the best platform for learning Malaysian Sign Language. 
+                    With expertise in technology, education, and accessibility, we work together to create an intuitive and inclusive learning experience for everyone. 
+                    Get to know the minds behind MudahJe and how they are shaping the future of sign language education!
                 </p>
             </div>
             <div class="row justify-content-center gap-6">
